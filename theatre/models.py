f515f1b2766c520c944b750333952de6ad6182f5
Maxim Turenko <turenkomaksim099@gmail.com>
2024-10-11 12:18:21 +0300

    add models TheatreHall Performance Reservation Ticket User(AbstactUser)

--- theatre/models.py
+++ theatre/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.utils import timezone
+from datetime import timedelta
+from django.core.exceptions import ValidationError
+
+from user_config.models import User
 
 
 class Actor(models.Model):
@@ -36,3 +42,75 @@ class Play(models.Model):
         ordering = ["title"]
 
 
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    rows = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    seats_in_rows = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    def clean(self):
+        super().clean()
+        if self.show_time < timezone.now() + timedelta(days=30):
+            raise ValidationError(
+                "The Performance date must be at least one month from today."
+            )
+
+    def __str__(self):
+        return (f'{self.play.title} - '
+                f'{self.show_time.strftime("%Y-%m-%d %H:%M")}')
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, related_name="reservations", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"Reserved by {self.user.email} on {self.created_at}"
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    seat = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    performance = models.ForeignKey(Performance, on_delete=models.PROTECT)
+    reservation = models.ForeignKey(Reservation, on_delete=models.PROTECT)
+
+    def clean(self):
+        super().clean()
+        theatre_hall = self.performance.theatre_hall
+        if self.row > theatre_hall.rows:
+            raise ValidationError(
+                f"Row {self.row} does not exist in {theatre_hall.name}."
+            )
+        if self.seat > theatre_hall.seats_in_rows:
+            raise ValidationError(
+                f"Seat {self.seat} does not exist in row "
+                f"{self.row} of {theatre_hall.name}."
+            )
+
+    def __str__(self):
+        return (f"Performance: {self.performance}, "
+                f"row: {self.row}, seat: {self.seat}")
+
+    class Meta:
+        unique_together = ("row", "seat", "performance")
